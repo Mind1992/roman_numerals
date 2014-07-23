0d0382073688f388c8ade9d036aeebd51832550a
Jeff Stringer & Sergii Mertsalov <student@epicodus.com>
2014-07-23 14:17:47 -0700

    added code to pass spec for 4,9

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,12 +1,19 @@
 def roman_numerals(num)
   roman_num = []
-  romans = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 }
+  romans = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 9 =>"IX", 5 => "V", 4 => "IV", 1 => "I"}
 
   romans.each do |k,v|
-    while num>=v
-      roman_num << k.to_s
-      num-=v
+    while num>=k
+        if num ==(k-1) && num != 0
+          rem = k%num
+          puts rem
+        roman_num.push(rem,v)
+        else
+          roman_num << v
+      end
+      num-=k
     end
   end
   roman_num.join
 end
+puts roman_numerals(49)
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,14 +3,27 @@ require 'roman_numerals'
 
 describe('roman_numerals') do
   it("returns the roman numeral I for the arabic number 1") do
-    expect(roman_numerals(1)).to eq("I");
+    expect(roman_numerals(1)).to eq("I")
   end
 
   it("returns the roman numeral II for the arabic number 2") do
-    expect(roman_numerals(2)).to eq("II");
+    expect(roman_numerals(2)).to eq("II")
   end
 
   it("returns the roman numeral III for the arabic number 3") do
-    expect(roman_numerals(3)).to eq("III");
+    expect(roman_numerals(3)).to eq("III")
+  end
+
+  it("returns the roman numeral IV for the arabic number 4") do
+    expect(roman_numerals(4)).to eq("IV")
+  end
+
+  it("returns the roman numeral VI for the arabic number 6") do
+    expect(roman_numerals(6)).to eq("VI")
+  end
+
+  it("returns the roman numeral VI for the arabic number 9") do
+    expect(roman_numerals(9)).to eq("IX")
   end
 end
+
